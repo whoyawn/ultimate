968a9b3af44cf26c232b7cb7c1265e76e5d67c61
Cherylyli <cherylliat@gmail.com>
2016-09-17 20:21:06 -0400

    able to change state via web

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -9,6 +9,19 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$timeout', function($scope, $timeout) {
+
+	firebase.database().ref('Group').on('value', function(snapshot){
+		if(!snapshot.val()) return;
+		$timeout(function(){
+			$scope.group = snapshot.val();
+		});
+
+	});
+
+	$scope.changemode = function(key, newState){
+		if (['time', 'follow', 'alarm'].indexOf(newState) === -1) return;
+		firebase.database().ref('Group/' + key + '/').update({state: newState});
+ 	}
 
 }]);
